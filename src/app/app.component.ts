04bedc25e1b2f17ea0ac5b982048fe20474172db
Diego Bruno <ogeid_hhh@hotmail.com>
2024-04-07 20:04:43 -0600

    Envía y recibe mensajes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocketioService } from './socketio.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'socketio-angular';
+
+  userID: String = "";
+  mensaje: String = "";
+  destinatario: String = "";
+  contrasenha: String = "";
+
+  mensajesRecibidos: String[] = []
+
+  estaConectado: boolean = false;
+
+  constructor(private socketService: SocketioService) { }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.socketService.disconnect();
+  }
+
+  conectarAlServidor() {
+    this.socketService.setupSocketConnection(this.userID);
+    this.estaConectado = true;
+
+    // Suscribirse al evento new_message para recibir mensajes del servidor
+    this.socketService.onNewMessage().subscribe((message: String) => {
+      this.mensajesRecibidos.push(message)
+      console.log('Mensaje recibido:', message);
+    });
+  }
+
+  enviarMensaje() {
+    console.log('Enviar mensaje a:', this.destinatario, ', Mensaje: ', this.mensaje);
+    this.socketService.sendMessageToUser(this.destinatario, this.mensaje);
+  }
+
+  generateKeyPair() {
+    console.log("Generando ECDSA")
+    this.socketService.generateECDSAKeyPair().subscribe((keyPair) => {
+      console.log('ECDSA key pair generated:', keyPair);
+      window.crypto.subtle.exportKey("jwk", keyPair.publicKey).then(publicKey => {
+        console.log('Public key:', publicKey);
+      })
+
+      window.crypto.subtle.exportKey("jwk", keyPair.privateKey).then(privateKey => {
+        console.log('Private key:', privateKey);
+      })
+    })
+  }
+
 }
+
+
---
+++ src/app/socketio.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketioService {
+  socket: any;
+
+  constructor() { }
+
+  setupSocketConnection(identificador: String) {
+    this.socket = io(environment.SOCKET_ENDPOINT);
+
+    this.socket.emit('setUserId', identificador); // Asignar un ID de usuario al socket
+
+    this.socket.emit('my message', `Hello there from Angular\'s client ${identificador}.`);
+    this.socket.on('my broadcast', (data: String) => {
+      console.log(data);
+    });
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+
+  sendMessageToUser(userId: String, message: String) {
+    // Enviar un mensaje a un cliente específico usando su ID de usuario
+    if (this.socket) {
+      this.socket.emit('messageToUser', { userId, message });
+      console.log("Enviado")
+    }
+  }
+
+  onNewMessage(): Observable<String> {
+    return new Observable<String>(observer => {
+      this.socket.on('new_message', (message: String) => {
+        observer.next(message);
+      });
+    });
+  }
+
+  generateECDSAKeyPair(): Observable<CryptoKeyPair> {
+    console.log("Generando par de claves desde el servicio");
+    return from(
+      window.crypto.subtle.generateKey(
+        {
+          name: 'ECDSA',
+          namedCurve: 'P-384'
+        },
+        true,
+        ['sign', 'verify']
+      )
+    );
+  }
+
+}
