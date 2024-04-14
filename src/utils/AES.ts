eefd74ed0a342eaf1b960c8f5cf298bf19fdd5d9
ErickJa <54383766+ErickJa@users.noreply.github.com>
2024-04-14 11:06:24 -0600

    Fix: AES decrypt

--- src/utils/AES.ts
+++ src/utils/AES.ts
@@ -1,13 +1,13 @@
 export async function encryptAES(derKey: CryptoKey, message:string, initV:Uint8Array) {
     // iv will be needed for decryption
     const msgBuffer = new TextEncoder().encode(message);
-    return crypto.subtle.encrypt({name:'AES-CBC',iv:initV},derKey, msgBuffer);
+    console.log('mensaje antes de encriptar:', decodifica(msgBuffer));
+    return await crypto.subtle.encrypt({name:'AES-CBC',iv:initV},derKey, msgBuffer);
 };
-export async function decryptAES(derKey: CryptoKey, message:string, initV:Uint8Array) {
+export async function decryptAES(derKey: CryptoKey, message:ArrayBuffer, initV:Uint8Array) {
     // iv will be needed for decryption
-    const msgBuffer = new TextEncoder().encode(message);
-    return crypto.subtle.decrypt({name:'AES-CBC',iv:initV},derKey, msgBuffer);
-    
+    console.log('mensaje encriptado:', decodifica(message));
+    return await crypto.subtle.decrypt({name:'AES-CBC',iv:initV},derKey, message);
 };
 //se mete la clave publica del receptor y la clave privada del emisor
 export function deriveKey(privateKey: CryptoKey, publicKey: CryptoKey){
@@ -35,3 +35,9 @@ export async function createPair() {
         ["deriveKey"],
     );
 };
+
+export function decodifica( buffer:ArrayBuffer ) {
+    const decoder = new TextDecoder();
+    const decodedMessage = decoder.decode(buffer);
+    return decodedMessage
+}
