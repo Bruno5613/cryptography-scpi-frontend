f03c3fd2cc33080551ebe75b438b0c00fede3bf2
carlaelizabeth <carla.r@comunidad.unam.mx>
2024-04-14 19:14:08 -0600

    fix typo string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   title = 'socketio-angular';
 
   userID: String = "";
-  mensaje: String = "";
+  mensaje: string = "";
   destinatario: String = "";
   userPass: String = "";
 
